b97607408842d711dd5d148e86d61b9cbe8d5569
Sergey Berdyshev <sberdyshev@cinimex.ru>
2019-01-29 17:30:07 +0300

    java doc refactoring

--- src/main/java/ru/sberdyshev/learn/geekbrains/java/core/professional/lesson6/homework/test/db/examples/second/entity/Student.java
+++ src/main/java/ru/sberdyshev/learn/geekbrains/java/core/professional/lesson6/homework/test/db/examples/second/entity/Student.java
@@ -1,5 +1,8 @@
 package ru.sberdyshev.learn.geekbrains.java.core.professional.lesson6.homework.test.db.examples.second.entity;
 
+/**
+ * @author sberdyshev
+ */
 public class Student {
     private String name;
     private String id;
